45f4affde55b874203a389ce0638ea74caa7f29a
alexanderDoria <adoria@csumb.edu>
2017-09-18 23:12:57 -0700

    reversed

--- fantasy.py
+++ fantasy.py
@@ -4,7 +4,6 @@ picks = []
 numTeams = 12
 
 #dictates how much weight to assign to lower numbers i.e. more fairness is less weight to lower numbers
-#recommended 2 - 4
 fairness = 2
 
 #init probabilities
@@ -12,6 +11,14 @@ for i in range(1, numTeams + 1):
     prob = int(1.0 / (i + 1) * 100)
     prob = prob + int(prob / fairness) + 1
     picks.extend([i] * prob)
+
+#check probabilities
+#sum = 0
+#for i in range(1, 13):
+#    print (i, ": ", picks.count(i))
+#    sum += picks.count(i)
+
+#print "sum: ", sum
 
 #pick
 print ("Welcome to Bilal and Samir's Yahoo NBA Fantasy League, 2017-2018!")
